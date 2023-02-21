e92771ba19467602287217af24b79253240df657
Justin Henricks <justinhenricks@gmail.com>
2023-02-20 21:47:37 -0500

    whitespace

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,7 +44,6 @@ module.exports = {
         ],
       ],
     },
-
     boxShadow: {
       DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.2), 0 1px 2px -1px rgb(0 0 0 / 0.2);",
       md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.3);",
